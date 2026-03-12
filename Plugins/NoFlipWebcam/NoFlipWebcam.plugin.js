83c70992590fe4047ff4553611b67ab91019b4aa
dededed6 <clasp-blur.5t@icloud.com>
2026-03-12 13:48:58 +0900

    Update NoFlipWebcam

--- Plugins/NoFlipWebcam/NoFlipWebcam.plugin.js
+++ Plugins/NoFlipWebcam/NoFlipWebcam.plugin.js
@@ -7,16 +7,9 @@
  * @source https://raw.githubusercontent.com/dededed6/BetterDiscordPlugins/master/NoFlipWebcam/NoFlipWebcam.plugin.js
  */
 
-const { DOM } = BdApi;
-const style = `
-[class*="mirror__"], [class^="camera__"]
-{
-    transform: scaleX(1) !important;
-}`;
-    
 module.exports = class NoFlipWebcam
 {
     constructor() {}
-    start() { DOM.addStyle("NoFlipWebcam", style); }
-    stop() { DOM.removeStyle("NoFlipWebcam"); }
+    start() { BdApi.DOM.addStyle("NoFlipWebcam", `[class*="mirror__"], [class^="camera__"] { transform: scaleX(1) !important; }`); }
+    stop() { BdApi.DOM.removeStyle("NoFlipWebcam"); }
 };
